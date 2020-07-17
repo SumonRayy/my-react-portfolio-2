7973d836f0f109040db82540170ef56d5360ff32
SumonRayy <sumonrayy@gmail.com>
2020-07-17 16:12:40 +0530

    Prothom Commit

---
+++ src/components/about.jsx
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-12">
+                <div
+                  className="row row-bottom-padded-sm animate-box"
+                  data-animate-effect="fadeInLeft"
+                >
+                  <div className="col-md-12">
+                    <div className="about-desc">
+                      <span className="heading-meta">About Us</span>
+                      <h2 className="colorlib-heading">Who Am I?</h2>
+                      <p>
+                        I am a computer grad student from DAIICT-Gandhinagar
+                        with ICT as my major.I love exploring new technologies
+                        and often amazed by the progress we as a human species
+                        have mad so far in the recent years (apart from the
+                        headphone-jack part!!)
+                      </p>
+                      <p>
+                        I have started reflecting my ideas and thougths through
+                        the medium of words recently so spelling and grammer
+                        mistaks are very often.You can write me back if you spot
+                        any and don't want to live anymore :P{" "}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="colorlib-about">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">
+                  Here are some of my expertise
+                </h2>
+              </div>
+            </div>
+            <div className="row row-pt-md">
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                  <span className="icon">
+                    <i className="icon-bulb" />
+                  </span>
+                  <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>
+                      I have experience building websites using ReactJS,
+                      Gatsby-JS, NodeJS, Wordpress etc.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                  <span className="icon">
+                    <i className="icon-database" />
+                  </span>
+                  <div className="desc">
+                    <h3>Data Structures & Algorithms</h3>
+                    <p>
+                      As coming from the CS background, I have good grasp over
+                      fundamental concepts of DSA.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                  <span className="icon">
+                    <i className="icon-video2" />
+                  </span>
+                  <div className="desc">
+                    <h3>Video Editing </h3>
+                    <p>
+                      I'm a Passionate Film Maker and Commercial Cinematic Video
+                      Maker.{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                  <span className="icon">
+                    <i className="icon-device-desktop" />
+                  </span>
+                  <div className="desc">
+                    <h3>Game Developer</h3>
+                    <p>A Beginner Indie Game Dev used Unity. </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                  <span className="icon">
+                    <i className="icon-layers2" />
+                  </span>
+                  <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>
+                      Have a Good Knowledge on Adobe Photoshop , Adobe Indesign
+                      , Adobe After Effects.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                  <span className="icon">
+                    <i className="icon-phone3" />
+                  </span>
+                  <div className="desc">
+                    <h3>Digital Marketing</h3>
+                    <p>I use Instagram eight hours a day :) </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Developer at Interior Traffic{" "}
+                          <span>2020-present</span>
+                        </h2>
+                        <p>
+                          I recently have joined the Interior Traffic as a
+                          Fullstack Website Developer. I've to Build an entire
+                          Web application totally on my own. I've used
+                          Technolgies Like Wordpress , Elementor etc. &
+                          Languages Like pHp , css , MySql .{" "}
+                          <a href="https://interiortraffic.com/">
+                            InteriorTraffic.com
+                          </a>{" "}
+                          is a place where You not only get the best & Latest
+                          Interior Design/Appliance ideas for Your Modern
+                          Luxurious Dream Home but also You get the Latest &
+                          Hottest Deals from World’s Leading Digital
+                          Marketplaces Shipping from Worldwide to Your Doorstep.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInTop"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Diploma Holder from Kanyapur Polytechnic
+                          <span> 2017-2020</span>
+                        </h2>
+                        <p>
+                          I am pursuing my Diploma studies with major in
+                          DCST(Diploma in Comuter Science and Technology). I
+                          have taken courses like DSA, OOPs, Computer Networks,
+                          IOT, Web-Data Management over the years and have
+                          better understanding of these subjects.I have also
+                          been part of C.R.(Class Representative) since my first
+                          year of college and T.P.R(Training and PlaceMents ).
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Primary and Higher Education <span>2005-2017</span>
+                        </h2>
+                        <p>
+                          I have completed my higher secondary education with
+                          major subjects as Physics, Chemistry & Maths with .
+                          During my time at school, I have developed interest in
+                          solving complex problems of the fundamental physics
+                          which helped me to improve my understanding of any
+                          problem and also my mathematics was my most Favourite
+                          subject which helped me alot to Build my Career.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
